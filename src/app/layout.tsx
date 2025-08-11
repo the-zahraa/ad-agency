f37af167afca2d0dd1ed43c5fcdd783c0a908af0
electron <eleproneuqua@gmail.com>
2025-08-12 02:37:57 +0330

    meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,6 +89,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', 'G-2GBKK2RGWC');
           `}
         </Script>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1538557934185142');
+          fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1538557934185142&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className="antialiased">
         <ScrollProvider>
